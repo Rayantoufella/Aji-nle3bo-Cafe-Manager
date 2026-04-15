66a203f064d6f67c77ff30144c5c639155d02a39
UssMAD <youssefmoumadd@gmail.com>
2026-04-15 15:09:45 +0100

    Session Model&Controller + Namespace

--- app/controller/SessionController.php
+++ app/controller/SessionController.php
@@ -1,5 +1,7 @@
 <?php
-require_once __DIR__ ."/../models/session.php";
+namespace App\Controllers;
+use App\Models\SessionsModel;
+use App\Models\DatabaseModel;
 
 // session_start();
 // if(!isset($_SESSION['user_id'])){
