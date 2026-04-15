bc7d2d5e4445560d900d492ebf19b30add1f173b
marouazouidi <zouidimaroua@gmail.com>
2026-04-15 15:04:20 +0100

    add namespace

--- app/models/DatabaseModel.php
+++ app/models/DatabaseModel.php
@@ -1,6 +1,9 @@
 <?php
-require_once __DIR__ . '/../../config/config.php';
+namespace App\Models;
 
+require_once __DIR__ . "/../../config/config.php";
+use PDO;
+use PDOException;
 
 class Database
 {
