481d25b2f0d30b2ef0d466246640dabe90295682
UssMAD <youssefmoumadd@gmail.com>
2026-04-15 12:10:32 +0100

    Session Controller update

--- app/controller/SessionController.php
+++ app/controller/SessionController.php
@@ -1,6 +1,12 @@
 <?php
 require_once __DIR__ ."/../models/session.php";
 
+// session_start();
+// if(!isset($_SESSION['user_id'])){
+    
+//         header('Location: index.php');
+//         exit();
+//     }
 class SessionsController {
 
     private $model;
