9be99c8e56dfadd243035c23a04c72040a976be0
Rayan Toufella <rayantoufella3@gmail.com>
2026-04-15 20:32:25 +0100

    User controler

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -21,42 +21,43 @@ class UserModel {
 
     }
 
-    public function login($email,$password){
-        try{
-                    $this->email = $email;
-        $this->password = $password;
-        
-        $stmt = $this->db->prepare("SELECT * FROM users WHERE email = :email") ; 
-        $stmt->execute(["email" => $this->email]) ; 
+    public function create($data){
+try{
+    $query = 'INSERT INTO users (username, email, password, role, created_at) VALUES (:username, :email, :password, :role, :created_at)' ;
+    $this->db->prepare($query) ;
+    $this->db->bindValue(':username', htmlspecialchars($data['username']), PDO::PARAM_STR) ;
+    $this->db->bindValue(':email', htmlspecialchars($data['email']), PDO::PARAM_STR) ;
+    $this->db->bindValue(':password', password_hash($data['password'], PASSWORD_BCRYPT), PDO::PARAM_STR) ;
+    $this->db->bindValue(':role', 'user', PDO::PARAM_STR) ;
+    $this->db->bindValue(':created_at', date('Y-m-d H:i:s'), PDO::PARAM_STR) ;
+}catch(PDOException $e){
+    exit ("Erreur  Connectionn " . $e->getMessage()) ;
 
-        $user = $stmt->fetch(PDO::FETCH_ASSOC) ;
-        
-        if($user && password_verify($this->password,$user['password'])){
-            return $user ;
-        }
+}
+
+    }
+    public function finByEmail($email){
+        try{
+            $query = 'SELECT email FROM users WHERE email = :email ' ; 
+            $this->db->prepare($query) ;
+            $this->db->bindValue(':email', $email, PDO::PARAM_STR) ;
+            $this->db->execute() ;
+            return $this->db->fetch(PDO::FETCH_ASSOC) ;
         }catch(PDOException $e){
-            return false ;
+            exit('Error fetching user by email' . $e->getMessage()) ;
         }
     }
 
-    public function register($username ,$email ,$password){
+    public function findById(){
         try{
-        $this->username = $username;
-        $this->email = $email;
-        $this->password = $password;
-
-        $query = "INSERT INTO users (username,email,password) VALUES (:username,:email,:password)" ;
-        $stmt = $this->db->prepare($query) ;
-        $stmt->execute(["username" => $this->username,"email" => $this->email,"password" => $this->password]) ;
-        return true ;
+            $query = 'SELECT * FROM users WHERE id = :id ' ;
+            $this->db->prepare($query) ;
+            $this->db->bindValue(':id', $this->id, PDO::PARAM_INT) ;
+            $this->db->execute() ;
+            return $this->db->fetch(PDO::FETCH_ASSOC) ;
         }catch(PDOException $e){
-            return false ;
+            exit('Error fetching user by id' . $e->getMessage()) ;
         }
-    }
-
-    public function logout(){
-        session_destroy();
-        header("Location: /Aji-nle3bo-Cafe-Manager/public/index.php");
     }
 
     
